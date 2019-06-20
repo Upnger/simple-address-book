9cad059fce81b91bb43bef734c9b3b13797dc918
Robin Persson <robin.persson@solitio.se>
2019-06-20 14:57:57 +0200

    Add eslint and prettier

---
+++ back-end/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  extends: ['plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {}
+}
