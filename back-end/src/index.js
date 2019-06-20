e67c125f04b27a0c672645f07a69043f5c818b0e
Robin Persson <robin.persson@solitio.se>
2019-06-20 16:12:59 +0200

    Add basic docker

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -46,7 +46,7 @@ fastify.put('/contact', async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen(3000, '0.0.0.0')
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
     fastify.log.error(err)
