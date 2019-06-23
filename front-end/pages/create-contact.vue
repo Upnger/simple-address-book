92566bcfab76cdb0061fd4cd072e906384b2a68d
Robin Persson <robin.persson@solitio.se>
2019-06-23 20:16:10 +0200

    Add view to create contact

---
+++ front-end/pages/create-contact.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="field">
+      <label class="label">Förnamn:</label>
+      <div class="control">
+        <input
+          v-model="newContact.firstName"
+          class="input"
+          type="text"
+          placeholder="Robin"
+        />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Efternamn:</label>
+      <div class="control">
+        <input
+          v-model="newContact.lastName"
+          class="input"
+          type="text"
+          placeholder="Persson"
+        />
+      </div>
+    </div>
+    <button class="button is-primary" @click="create">Skapa</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+const newContactDefaultValues = {
+  firstName: '',
+  lastName: ''
+}
+export default {
+  data: function() {
+    return {
+      newContact: newContactDefaultValues
+    }
+  },
+  methods: {
+    create: function() {
+      return axios
+        .post('http://localhost:3000/contact', this.newContact)
+        .then(res => {
+          this.contacts = newContactDefaultValues
+          alert('Kontakt sparad')
+        })
+        .catch(function() {
+          alert('failed to create new contact')
+        })
+    }
+  }
+}
+</script>
